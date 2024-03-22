7266f0146bfb8c10bae9c2db0deb3ef4d32e3409
dimenatar <dimenatar25@gmail.com>
2024-03-22 20:18:03 +0300

    Setupped scene

--- Assets/Scripts/EnvironmentInfiniteSpawner.cs
+++ Assets/Scripts/EnvironmentInfiniteSpawner.cs
@@ -1,27 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
-[System.Serializable]
-public struct Environment
-{
-    [SerializeField] private Transform _transform;
-    [SerializeField] private float _length;
-    [SerializeField] private int _startAmountOnLevel;
 
-    public int timesSpawned;
-
-    public Transform Transform { get => _transform; private set => _transform = value; }
-    public float Length { get => _length; private set => _length = value; }
-    public int StartAmountOnLevel {get => _startAmountOnLevel; private set => _startAmountOnLevel = value; }
-}
 
 public class EnvironmentInfiniteSpawner : MonoBehaviour
 {
-    [SerializeField] private Transform _train;
-    [SerializeField] private List<Environment> _environmentList;
-
-    private float _passedDistance;
+    [SerializeField] private Transform _parent;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _offset;
+    [SerializeField] private int _spawningAmount;
 
     private void Awake()
     {
@@ -31,5 +18,15 @@ public class EnvironmentInfiniteSpawner : MonoBehaviour
     private void Update()
     {
         
+    }
+
+    [Button]
+    public void Spawn()
+    {
+        for (int i = 0; i < _spawningAmount; i++)
+        {
+            var copy = Instantiate(_prefab, _parent);
+            copy.transform.position = new Vector3(i * _offset, 0f, 0f);
+        }
     }
 }
